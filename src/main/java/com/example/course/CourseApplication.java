af64c8538adf825d592b808d7805fd2bf12a4b33
Arthur Pulini <arthurpulini@gmail.com>
2023-09-06 12:13:48 -0300

    OrderStatus enum

--- src/main/java/com/example/course/CourseApplication.java
+++ src/main/java/com/example/course/CourseApplication.java
@@ -3,6 +3,8 @@ package com.example.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.course.entities.enums.OrderStatus;
+
 @SpringBootApplication
 public class CourseApplication {
 
